072bcdce59ccce618cd7859245ceb7214099441e
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-15 01:30:59 +0300

    add views for Habit

--- habit/views.py
+++ habit/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Habit
+from .serializers import HabitSerializer
 
-# Create your views here.
+
+class HabitListCreateView(generics.ListCreateAPIView):
+    """
+    Представление для создания и просмотра списка привычек пользователя.
+    """
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Возвращаем только привычки текущего пользователя
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # Автоматически устанавливаем пользователя при создании привычки
+        serializer.save(user=self.request.user)
+
+
+class HabitDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Представление для просмотра, обновления и удаления привычки.
+    """
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Возвращаем только привычки текущего пользователя
+        return Habit.objects.filter(user=self.request.user)
