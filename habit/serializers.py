3ab234335f5111df9c848ffb8ec6cb50c94d0c49
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-17 11:41:02 +0300

    add validator related_habit

--- habit/serializers.py
+++ habit/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from habit.models import Habit
+from habit.validators import validate_related_habit
 
 
 class HabitSerializer(serializers.ModelSerializer):
@@ -11,3 +12,10 @@ class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+
+    def validate(self, data):
+        """
+        Валидаторы для модели Habit.
+        """
+        validate_related_habit(data)
+
