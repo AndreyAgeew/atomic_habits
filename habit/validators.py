a289acbd13f29f8718ae2fb4f9716ef8b7ea0f8d
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-23 23:12:53 +0300

    changes validate_related_habit (value['reward'])

--- habit/validators.py
+++ habit/validators.py
@@ -6,7 +6,7 @@ def validate_related_habit(value):
     """
     Валидатор: Исключить одновременный выбор связанной привычки и указания вознаграждения.
     """
-    if value['related_habit'] and value.reward:
+    if value['related_habit'] and value['reward']:
         raise serializers.ValidationError("You can't choose both a related habit and a reward.")
 
 
