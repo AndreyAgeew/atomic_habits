3fbb618fe9ddbdd00b71e0388d6fbefa2c23ed26
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-24 13:23:00 +0300

    add CELERY_BEAT_SCHEDULE (send_mailing_message) (timedelta(seconds=10) for tests)

--- atomic_habits/settings.py
+++ atomic_habits/settings.py
@@ -182,8 +182,16 @@ CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
 CELERY_TASK_TRACK_STARTED = True
-CELERY_TASK_TIME_LIMIT = 30 * 60
+# CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Europe/Moscow'
+
+# Настройки для Celery
+CELERY_BEAT_SCHEDULE = {
+    'task-name': {
+        'task': 'habit.tasks.send_mailing_message',  # Путь к задаче
+        'schedule': timedelta(seconds=10),  # Расписание выполнения задачи (например, каждые 10 минут)
+    },
+}
